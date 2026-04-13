ff6a36df939ea545916a5ed55493832e6eb4897e
itsmonskers <422000678@ntc.edu.ph>
2026-04-13 23:14:06 +0800

    Add files via upload

---
+++ netlify/functions/notion.js
@@ -0,0 +1,35 @@
+exports.handler = async function (event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const NOTION_TOKEN = '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';
+  const NOTION_VER   = '2022-06-28';
+
+  try {
+    const body = JSON.parse(event.body);
+    const { endpoint, payload } = body;
+
+    const notionRes = await fetch(`https://api.notion.com/v1/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Notion-Version': NOTION_VER,
+        'Authorization': 'Bearer ' + NOTION_TOKEN,
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const data = await notionRes.json();
+    return {
+      statusCode: notionRes.status,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: err.message }),
+    };
+  }
+};
